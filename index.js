4989797e9552c84ac7aeb6b0db8a79c665724dd5
Zandi-Galpin <zandi-galpin@live.nmit.ac.nz>
2025-02-24 11:21:13 +1300

    practising variables and operators

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
 console.log("Zandi Galpin")
 console.log(2+1)
+
+//ES5 (old version of javascript)
+var a = 2
+
+//ES6 (new version of javascript)
+const b = 3
+let c = 4
+
+let num1 = 51
+let num2 = 10
+const result = num1 * num2
+console.log("result = "+result) 
+console.log("variable a = "+a+" and variable b = "+b+" so a + b = "+(a+b))
+
+let num3 = 500
+let num4 = 5
+const result2 = num3 / num4
+console.log(num3+" / "+num4+" = "+result2) 
+
 //learning JS in SDV503 class
 
 /**
